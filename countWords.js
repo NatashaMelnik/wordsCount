78b854d7a6da952f0d6ff19f264ea9498b7dcf2d
NatashaMelnik <n.melnik1788@gmail.com>
2021-07-21 21:27:09 +0300

    words counting

--- countWords.js
+++ countWords.js
@@ -1,19 +1,29 @@
 function CountWords(str) {
+
     let wordsArr = CreateCorrectWordsArr(str);
 
     let wordsCount = new Map();
 
     for (let i = 0; i < wordsArr.length; i++) {
-        if (!wordsCount.has[wordsArr[i]]) {
+        if (!wordsCount.has(wordsArr[i])) {
             wordsCount.set(wordsArr[i], 1);
         }
         else {
-            wordsCount.set(wordsArr[i], 2);
+            let currNum = +wordsCount.get(wordsArr[i]) + 1;
+            wordsCount.delete(wordsArr[i]);
+            wordsCount.set(wordsArr[i], currNum);
         }
     }
 
-    return wordsCount
+    PrintResult(wordsCount);
 
+}
+
+function PrintResult(map) {
+    map.forEach((value, key,) => {
+        console.log(`${key}: ${value}`);
+    });
+    console.log('__________');
 }
 
 function CreateCorrectWordsArr(str) {
